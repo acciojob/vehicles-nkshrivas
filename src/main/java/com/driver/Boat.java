868332d90db4a73a20db381a0d3754caeacaa17e
nkshrivas <nk733095@gmail.com>
2023-03-18 17:06:40 +0530

    boat ready to race 2

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,17 +2,16 @@ package com.driver;
 
 public class Boat implements WaterVehicle{
 
-    private String name;
-    private int capacity;
+    public String name;
+    public int capacity;
 
 
-    Boat(String name,int capacity){
+   public Boat(String name,int capacity){
         this.name=name;
         this.capacity=capacity;
     }
     @Override
     public String getVehicleName() {
-
         return name;
     }
 
